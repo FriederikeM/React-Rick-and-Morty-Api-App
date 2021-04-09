b92d84b07c7857692e4d3d201975548dae9dba39
Friederike Moeser <friederike.moeser@gmail.com>
2021-04-09 22:30:10 +0200

    Add filter form

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,3 +1,22 @@
 export default function Filter() {
-  return "Filter here";
+  return (
+    <div className="filter-form-container">
+      <form className="filter-form">
+        <label htmlFor="character-name">Name</label>
+        <input
+          type="text"
+          name="character-name"
+          id="character-name"
+          placeholder="Search for you favourite character"
+        />
+        <label htmlFor="status">Status</label>
+        <select name="status" id="status">
+          <option value="all">All</option>
+          <option value="dead">Dead</option>
+          <option value="alive">Alive</option>
+          <option value="unknown">Unknown</option>
+        </select>
+      </form>
+    </div>
+  );
 }
