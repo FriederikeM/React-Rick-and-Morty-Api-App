7c5bfc694ad327eb8c454836b06f103f0fcdf84b
Friederike Moeser <friederike.moeser@gmail.com>
2021-04-10 22:57:06 +0200

    Add media queries

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,18 @@
 import wallpaper from "../images/wallpaper.png";
+import "./Home.css";
 
 export default function Home() {
   return (
     <div className="welcome">
-      <h3>Welcome to the Rick and Morty App</h3>
-      <img src={wallpaper} alt="rick and morty riding in a car" width="100%" />
-      <h5>Developedby Rikki during the Neue Fische Web Dev Bootcamp</h5>
+      <h3 className="welcome-message">Welcome to the Rick and Morty App</h3>
+      <img
+        className="welcome-img hidden-small"
+        src={wallpaper}
+        alt="rick and morty riding in a car"
+      />
+      <h5 className="credits">
+        Developed by Rikki during the Neue Fische Web Dev Bootcamp
+      </h5>
     </div>
   );
 }
