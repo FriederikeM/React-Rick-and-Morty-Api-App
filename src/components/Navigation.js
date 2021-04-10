4cc89d060c31d7a78088acfab69db30719375d32
Friederike Moeser <friederike.moeser@gmail.com>
2021-04-10 22:05:16 +0200

    Style header and adjust mobile header

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,24 @@
 import { NavLink } from "react-router-dom";
 import "./Navigation.css";
+import logo from "../images/rick-and-morty-logo.png";
 
 export default function Navigation() {
   return (
-    <nav className="navigation">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/characters">Characters</NavLink>
-    </nav>
+    <div className="header-wrapper">
+      <img
+        className="logo hidden-mobile"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Rick_and_Morty.svg/799px-Rick_and_Morty.svg.png"
+        alt="rick and morty logo"
+      />
+      <img
+        className="logo hidden-desktop"
+        src={logo}
+        alt="rick and morty logo"
+      />
+      <nav className="navigation">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/characters">Characters</NavLink>
+      </nav>
+    </div>
   );
 }
