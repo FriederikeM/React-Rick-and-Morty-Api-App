3a9769c5688a20a17d5d566116dfd1a075e572d3
Friederike Moeser <friederike.moeser@gmail.com>
2021-04-10 13:27:13 +0200

    Add functionality to status filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,6 +1,11 @@
 import "./Filter.css";
 
-export default function Filter() {
+export default function Filter({ onFilterChange }) {
+  function handleFilterSelection(event) {
+    const filterValue = event.target.value;
+    onFilterChange(filterValue);
+  }
+
   return (
     <div className="filter-form-container">
       <form className="filter-form">
@@ -12,12 +17,13 @@ export default function Filter() {
           placeholder="Search for you favourite character"
         />
         <label htmlFor="status">Status</label>
-        <select name="status" id="status">
+        <select onClick={handleFilterSelection} name="status" id="status">
           <option value="all">All</option>
           <option value="dead">Dead</option>
           <option value="alive">Alive</option>
           <option value="unknown">Unknown</option>
         </select>
+        <button type="submit" className="filter-form-button"></button>
       </form>
     </div>
   );
