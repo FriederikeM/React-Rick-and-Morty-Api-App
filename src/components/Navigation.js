36e5f81cdb764941c7ed8adc64c2ed500d283486
Friederike Moeser <friederike.moeser@gmail.com>
2021-05-15 12:30:27 +0200

    Rename class

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ import logo from "../images/rick-and-morty-logo.png";
 
 export default function Navigation() {
   return (
-    <div className="header-wrapper">
+    <div className="navigation-wrapper">
       <img
         className="logo hidden-mobile"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Rick_and_Morty.svg/799px-Rick_and_Morty.svg.png"
